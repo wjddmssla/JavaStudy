cb0eb6d675b81b06d7d51a2efe5e7977791b1b7b
wjddmssla <pakrose0108@naver.com>
2025-05-23 21:19:24 +0900

    14_lombok

---
+++ src/j14_lombok/Builder.java
@@ -0,0 +1,35 @@
+package j14_lombok;
+
+public class Builder extends CarBuilder {
+	
+	@Override
+	public CarBuilder builder() {
+		// TODO Auto-generated method stub
+		return super.builder();
+	}
+	
+	@Override
+	public Car build() {
+		// TODO Auto-generated method stub
+		return super.build();
+	}
+
+	@Override
+	public CarBuilder company(String company) {
+		car.setCompany(company);
+		return this;
+	}
+
+	@Override
+	public CarBuilder model(String model) {
+		car.setModel(model);
+		return this;
+	}
+
+	@Override
+	public CarBuilder color(String color) {
+		car.setColor(color);
+		return this;
+	}
+	
+}
---
+++ src/j14_lombok/Car.java
@@ -0,0 +1,17 @@
+package j14_lombok;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class Car {
+	private String company;
+	private String model;
+	private String color;
+	
+}
